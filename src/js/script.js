631b28eb554b2fd02a7f2f5eec85b389816a4f1c
Roma <romcheggggg@gmail.com>
2022-11-17 13:57:52 +0300

    sacha_pomogi

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
-    $('.carousel__inner').slick(
-        {
-            infinite: true,
-          }
-    );
+    $('.carousel__inner').slick({
+        infinite: true,
+        speed: 1200,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>'
+    });
 });
